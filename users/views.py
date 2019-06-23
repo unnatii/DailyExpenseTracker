b92a12ea802b1ce17f015c66eacb24882e892df6
unnatii <unnatitibrewal@gmail.com>
2019-06-23 11:15:23 +0530

    app

---
+++ users/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+# Create your views here.
+
+def register(request):
+    if request.method=='POST':
+         form=UserRegisterForm(request.POST)
+         if form.is_valid():
+             form.save()
+             username=form.cleaned_data.get('username')
+             messages.success(request,"Registered Successfully!Now you can Log-in!")
+             return redirect('login')
+    else:
+         form=UserRegisterForm()   
+    return render(request,'users/register.html',{'form':form})
+    
